ad8d6da3ed4e64bf2c243356224c7e1c7cfbec07
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-04-29 11:20:49 -0300

    urls and views

---
+++ recipes/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+#from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('home')
+
+
+def contato(request):
+    return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
