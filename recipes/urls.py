dba627722a1fb7dd98689893b4f58bf2be0f5661
Vinicius Martins <vinicius_marp500@hotmail.com>
2022-05-03 16:53:05 -0300

    view-recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,9 @@
+import django
 from django.urls import path
 
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),
-
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
